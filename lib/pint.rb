9b5a84f8883dd9c415dc075a62f7895d5ef768e1
Hadi <hadichalabi@gmail.com>
2014-10-01 16:35:46 +0100

    added break and full methods

--- lib/pint.rb
+++ lib/pint.rb
@@ -6,10 +6,18 @@ def initialize(name)
 	@name = name
 	end
 
+def broken?
+	@broken = false
+end
+
 def full?
 	@full = true
 	end
 
+def break
+	@break = true
+	end
+	
 def self.stella
 	new("stella")
 end
@@ -17,4 +25,6 @@ end
 def self.kronenberg
 	new("kronenberg")
 	end
+
+
 end
--- spec/pint_spec.rb
+++ spec/pint_spec.rb
@@ -3,10 +3,16 @@ require 'pint'
 describe Pint do
 
 let(:pint){Pint.new("stella")}
+let(:punter){double :punter}
 
-	it "should be full" do
+	it "should not be broken" do
+		expect(pint.broken?).to eq false
+	end
+
+	it "should not be full" do
 		expect(pint.full?).to eq true
 	end
+
 
 	it "should have a name" do
 		lager = Pint.new("stella")
@@ -21,5 +27,10 @@ let(:pint){Pint.new("stella")}
 	it "should be a Kronenberg" do
 		kronenberg = Pint.kronenberg
 		expect(kronenberg.name).to eq ("kronenberg")
+	end
+
+	it "should be able to break" do
+		allow(punter).to receive(:break) { pint }
+		expect(pint.break).to eq true
 	end
 end
